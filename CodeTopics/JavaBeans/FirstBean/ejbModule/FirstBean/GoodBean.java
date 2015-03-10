b38af6bbb2bf82f912537069126d40abae6c3b78
Aaron Smith <aaronwsmith12@gmail.com>
2015-03-09 21:20:09 -0600

    First JavaBeans

---
+++ CodeTopics/JavaBeans/FirstBean/ejbModule/FirstBean/GoodBean.java
@@ -0,0 +1,53 @@
+package FirstBean;
+
+import java.rmi.RemoteException;
+
+import javax.ejb.EJBException;
+import javax.ejb.LocalBean;
+import javax.ejb.SessionBean;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class GoodBean
+ */
+@Stateless
+@LocalBean
+public class GoodBean implements SessionBean {
+
+    /**
+     * Default constructor. 
+     */
+    public GoodBean() {
+    	
+    }
+    
+    public int addition(int a, int b) {
+    	return a+b;
+    }
+
+	@Override
+	public void ejbActivate() throws EJBException, RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ejbPassivate() throws EJBException, RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ejbRemove() throws EJBException, RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSessionContext(SessionContext arg0) throws EJBException,
+			RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+}
