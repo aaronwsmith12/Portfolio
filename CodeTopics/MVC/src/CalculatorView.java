8a679f8ed370576c40d977a827f13905961db28e
Aaron Smith <aaronwsmith12@gmail.com>
2015-03-21 21:44:00 -0600

    first MVC

---
+++ CodeTopics/MVC/src/CalculatorView.java
@@ -0,0 +1,89 @@
+// This is the View
+	// Its only job is to display what the user sees
+	// It performs no calculations, but instead passes
+	// information entered by the user to whomever needs
+	// it.
+	 
+	import java.awt.event.ActionListener;
+	 
+	import javax.swing.*;
+	 
+	public class CalculatorView extends JFrame{
+	 
+	    private JTextField firstNumber  = new JTextField(10);
+	    private JLabel additionLabel = new JLabel("+");
+	    private JTextField secondNumber = new JTextField(10);
+	    private JButton calculateButton = new JButton("Calculate");
+	    private JTextField calcSolution = new JTextField(10);
+	    private JLabel numberOne = new JLabel("1");
+	    private JLabel numberTwo = new JLabel("2");
+	     
+	    CalculatorView(){
+	         
+	        // Sets up the view and adds the components
+	         
+	        JPanel calcPanel = new JPanel();
+	         
+	        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        this.setSize(400, 400);
+	        numberOne.setLocation(10, 10);
+	         
+	        calcPanel.add(firstNumber);
+	        calcPanel.add(additionLabel);
+	        calcPanel.add(secondNumber);
+	        calcPanel.add(calculateButton);
+	        calcPanel.add(calcSolution);
+	        calcPanel.add(numberOne);
+	        calcPanel.add(numberTwo, calcPanel);
+//	        numberOne.setLocation(150, 150);
+//	        numberOne.add(new JButton("1"));
+	         
+	        this.add(calcPanel);
+	         
+	        // End of setting up the components --------
+	         
+	    }
+	     
+	    public int getFirstNumber(){
+	         
+	        return Integer.parseInt(firstNumber.getText());
+	         
+	    }
+	     
+	    public int getSecondNumber(){
+	         
+	        return Integer.parseInt(secondNumber.getText());
+	         
+	    }
+	     
+	    public int getCalcSolution(){
+	         
+	        return Integer.parseInt(calcSolution.getText());
+	         
+	    }
+	     
+	    public void setCalcSolution(int solution){
+	         
+	        calcSolution.setText(Integer.toString(solution));
+	         
+	    }
+	     
+	    // If the calculateButton is clicked execute a method
+	    // in the Controller named actionPerformed
+	     
+	    void addCalculateListener(ActionListener listenForCalcButton){
+	         
+	        calculateButton.addActionListener(listenForCalcButton);
+	         
+	    }
+	     
+	    // Open a popup that contains the error message passed
+	     
+	    void displayErrorMessage(String errorMessage){
+	         
+	        JOptionPane.showMessageDialog(this, errorMessage);
+	         
+	    }
+	     
+	}
+
